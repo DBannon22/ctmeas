21a575bfa0304c05fe9c92426f2c82488fe9c899
DBannon22 <dbannon22@gmail.com>
2025-03-13 10:42:53 -0400

    Update Leaflet.PolylineMeasure.js

--- Leaflet.PolylineMeasure.js
+++ Leaflet.PolylineMeasure.js
@@ -660,9 +660,9 @@
                     dist = (dist/1000).toFixed(2);
                 } else {
                     if (!this.options.useSubunits) {
-                        dist = (dist/10000).toFixed(4);
+                        dist = (dist/1000).toFixed(4);
                     } else {
-                        dist = (dist).toFixed(1);
+                        dist = (dist).toFixed(2);
                         unit = this.options.unitControlLabel.metres;
                     }
                 }
